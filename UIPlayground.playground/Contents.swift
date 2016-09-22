aa5486dce17fe61f5a1fa3fdf396853f28ddaefd
jackopuff <jcar8281@740s-pl-128.local>
2016-09-22 13:33:12 -0600

    added borders to UIView

--- UIPlayground.playground/Contents.swift
+++ UIPlayground.playground/Contents.swift
@@ -5,4 +5,6 @@ import UIKit
 var UserInterface = UIView(frame: CGRectMake(100,200,100,100))
 UserInterface.backgroundColor = UIColor.redColor()
 UserInterface.layer.cornerRadius=100
+UserInterface.layer.borderWidth=10
 UserInterface
+
