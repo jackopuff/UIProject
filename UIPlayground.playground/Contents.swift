b8a1bf86d86bd9b7b894c67cee9ccc5a853758a7
jackopuff <jcar8281@740s-pl-128.local>
2016-09-28 12:23:54 -0600

    added a second UIView, with a shadow

--- UIPlayground.playground/Contents.swift
+++ UIPlayground.playground/Contents.swift
@@ -3,8 +3,19 @@
 import UIKit
 
 var UserInterface = UIView(frame: CGRectMake(100,200,100,100))
+var NewUI = UIView(frame: CGRectMake(1,1,30,100))
+var ThirdUI = 
+let myColor : UIColor = UIColor (red:0, green: 1, blue:1, alpha: 0.6)
+let shadowColor : UIColor = UIColor(red:0.2, green:0.8, blue:0.5, alpha: 1)
+UserInterface.backgroundColor = UIColor.blackColor()
+
+NewUI.layer.borderWidth=10
+NewUI.layer.borderColor = myColor.CGColor
+NewUI.layer.shadowOpacity=1
+NewUI.layer.shadowRadius=25.0
+NewUI.layer.shadowColor = shadowColor.CGColor
+NewUI
 UserInterface.backgroundColor = UIColor.redColor()
 UserInterface.layer.cornerRadius=100
 UserInterface.layer.borderWidth=10
 UserInterface
-
